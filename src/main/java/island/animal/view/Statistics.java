163d703bd362dba71eb8fea471f16418e18965ca
Artemiy Laptev <artemiy.laptev@gmail.com>
2023-06-10 23:14:04 +0300

    10.06 23:13 statistics

--- src/main/java/island/animal/view/Statistics.java
+++ src/main/java/island/animal/view/Statistics.java
@@ -1,4 +1,7 @@
-package island.animal.model;
+package island.animal.view;
+
+import island.animal.model.Island;
+import java.text.DecimalFormat;
 
 public class Statistics implements Runnable{
     private Island island;
@@ -12,17 +15,31 @@ public class Statistics implements Runnable{
         printArray();
     }
 
+    DecimalFormat myFormat = new DecimalFormat("#.##");
+
     public void printArray() {
         int i = 0;
         int k = island.getWidth();
         for (int j = 0; j < island.getWidth(); j++) {
-            System.out.print("+-----------");
+            System.out.print("+--------------");
         }
         System.out.print("+");
         System.out.println();
         for (int j = 0; j < island.getHeight(); j++) {
             while (i < k) {
-                System.out.printf("|        %2d ", island.arrayCells[i].getAnimals().size());
+                System.out.printf("|Predator   %2d ", (int)island.arrayCells[i].getAnimals().stream().filter(x -> x.getSpecies().parentType.equals("Predator")).count());
+                i++;
+            }
+            i -= island.getWidth();
+            System.out.println("|");
+            while (i < k) {
+                System.out.printf("|Omnivorous %2d ", (int)island.arrayCells[i].getAnimals().stream().filter(x -> x.getSpecies().parentType.equals("Omnivorous")).count());
+                i++;
+            }
+            i -= island.getWidth();
+            System.out.println("|");
+            while (i < k) {
+                System.out.printf("|Herbivore  %2d ", (int)island.arrayCells[i].getAnimals().stream().filter(x -> x.getSpecies().parentType.equals("Herbivore")).count());
                 i++;
             }
             System.out.print("|");
@@ -31,16 +48,16 @@ public class Statistics implements Runnable{
             while (i < k) {
                 double plantCount = island.arrayCells[i].getPlantCount();
                 if (plantCount == 0) {
-                    System.out.printf("| %2f ", plantCount);
+                    System.out.printf("|              ", myFormat.format(plantCount));
                 } else {
-                    System.out.printf("|🌱%2f ", plantCount);
+                    System.out.printf("|🌱         %2s ", myFormat.format(plantCount));
                 }
                 i++;
             }
             System.out.println("|");
             k += island.getWidth();
             for (int l = 0; l < island.getWidth(); l++) {
-                System.out.print("+-----------");
+                System.out.print("+--------------");
             }
             System.out.println("+");
         }
